b7ff895303e1b983810b948b25adb99a7c3d40d7
xzyylx00 <xzyylx00@0918273.xyz>
2026-08-27 19:16:05 +0800

    chore: Prepare for tests

--- script/clean.sh
+++ script/clean.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-
-
 set -euo pipefail
 
 DRY_RUN=0
---
+++ test/format/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+tmp_dir = $(mktemp -d);
+
+testAndCheck() {
+        cp "$1" "$1.result" $tmp_dir
+        cd $tmp_dir
+        ./ctrc fmt $1
+        if [ -n `diff -q $1 $1.result` ]; then
+        else
+                exit -1;
+        fi
+}
+
+testAndCheck "1.ctr"
