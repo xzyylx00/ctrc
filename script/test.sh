51b0b1668211ae8a8af0a191db345baee410681c
xzyylx00 <xzyylx00@0918273.xyz>
2026-08-28 15:59:20 +0800

    chore: Stage codes

--- script/test.sh
+++ script/test.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+tmp_dir = $(mktemp -d);
+
+cp ./test/* $tmp_dir -r;
+cp ./zig-out/bin/* $tmp_dir -r;
+
+cd $tmp_dir;
+bash ./format/test.sh;
--- test/format/test.sh
+++ test/format/test.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 testAndCheck() {
-        cp "$1" "$1.result" $tmp_dir
-        cd $tmp_dir
-        ./ctrc fmt $1
+        cp "$1" "$1.result" $tmp_dir;
+        cd $tmp_dir;
+        ../ctrc fmt $1;
         if [ -n `diff -q $1 $1.result` ]; then
         else
                 exit -1;
         fi
 }
 
-testAndCheck "1.ctr"
+testAndCheck "1.ctr";
